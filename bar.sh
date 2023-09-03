0f9ea1c98e4acda0dbe46db6577c6d231f1819e2
ivan <47028390+ivanrg99@users.noreply.github.com>
2023-09-03 16:21:56 +0200

    Add files via upload

---
+++ bar.sh
@@ -0,0 +1,12 @@
+#!bin/sh
+#CODE: LYOKO
+
+while :; do
+	MEM="MEM: $(free -h | awk '/^Mem:/ {print $3 "/" $2}')"
+	DATE="$(date +'%b %d/%m %H:%M')"
+	SSID="$(hostname -i)"
+	BAT="BAT: $(cat /sys/class/power_supply/BAT0/capacity)%"
+    xsetroot -name "[$MEM] [$SSID] [$DATE] [$BAT]"
+
+	sleep 10s
+done
